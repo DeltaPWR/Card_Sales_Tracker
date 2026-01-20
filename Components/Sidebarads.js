00ff3b6ef2db713dfb03afe5b10029aeecd81fdc
DeltaPWR <trymebitc0in@gmail.com>
2026-01-20 00:53:27 -0500

    Create Sidebarads.js

---
+++ Components/Sidebarads.js
@@ -0,0 +1,17 @@
+export default function SidebarAds() {
+  return (
+    <aside className="sidebar">
+      <h4>Sponsored</h4>
+
+      <div className="ad">
+        <p>🔥 Sports Card Packs</p>
+        <a href="#" target="_blank">Shop on Amazon</a>
+      </div>
+
+      <div className="ad">
+        <p>📦 Card Sleeves & Toploaders</p>
+        <a href="#" target="_blank">View Deals</a>
+      </div>
+    </aside>
+  );
+}
